158c4ba4071c7ef1726e1fae2248bb482ddd8c34
unknown <interstellarpf>
2020-08-21 20:13:22 +0200

    remove firebase link from cors and added to heroku settings

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.PUBLIC_DOMAIN, "https://travel-with-a163a.web.app"],
+    origin: [process.env.PUBLIC_DOMAIN],
   })
 );
 // app.use((req, res, next) => {
