7616a13a1294f64c28f45c67d27777023357cafb
unknown <interstellarpf>
2020-08-24 21:24:34 +0200

    add upload route for uploading files

--- routes/upload.js
+++ routes/upload.js
@@ -3,7 +3,6 @@ const uploadRouter = express.Router();
 const createError = require("http-errors");
 const parser = require("./../config/cloudinary");
 
-
 // helper functions
 const {
   isLoggedIn,
@@ -11,16 +10,28 @@ const {
   validationLoggin,
 } = require("../helpers/middlewares");
 
-uploadRouter.post("/upload", parser.single("coverPic"), (req, res, next) => {
-    // console.log('file is: ', req.file)
-  
-    if (!req.file) {
-      next(new Error("No file uploaded!"));
-      return;
-    }
-    // get secure_url from the file object and save it in the
-    // variable 'secure_url', but this can be any name, just make sure you remember to use the same in frontend
-    res.json({ secure_url: req.file.secure_url });
-  });
-  
+uploadRouter.post("/", parser.single("profilePic"), (req, res, next) => {
+  // console.log('file is: ', req.file)
+
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  // get secure_url from the file object and save it in the
+  // variable 'secure_url', but this can be any name, just make sure you remember to use the same in frontend
+  res.json({ secure_url: req.file.secure_url });
+});
+
+uploadRouter.post("/coverpic", parser.single("coverPic"), (req, res, next) => {
+  // console.log('file is: ', req.file)
+
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  // get secure_url from the file object and save it in the
+  // variable 'secure_url', but this can be any name, just make sure you remember to use the same in frontend
+  res.json({ secure_url: req.file.secure_url });
+});
+
 module.exports = uploadRouter;
