dbe8b2ae18e8088139ebfed2c503fdc3f7525d3b
Marina Urbano <murbanobouterin@gmail.com>
2020-12-17 14:02:14 +0100

    heroku pruebas deploy

---
+++ public/precache-manifest.fbbab7ebfb79a3808adf42621c3f098c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9252b36df19a96e9c5d738019a64b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aff7ab19a8c9524670d3",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "daaa88fac70ba01214c5",
+    "url": "/static/css/main.09831df5.chunk.css"
+  },
+  {
+    "revision": "aff7ab19a8c9524670d3",
+    "url": "/static/js/2.2a516a6e.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/static/js/2.2a516a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daaa88fac70ba01214c5",
+    "url": "/static/js/main.2e002cdd.chunk.js"
+  },
+  {
+    "revision": "c02ab987d9618cf2d8f8",
+    "url": "/static/js/runtime-main.a7147f2e.js"
+  },
+  {
+    "revision": "8c89da3c2feabcfb6349106cb548bf82",
+    "url": "/static/media/womens.8c89da3c.jpg"
+  }
+]);
